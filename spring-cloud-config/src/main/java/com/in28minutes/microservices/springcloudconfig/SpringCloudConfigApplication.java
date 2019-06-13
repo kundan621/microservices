b6d503202e6ab01884068455b781343053339e58
kundan <kundan621@gmail.com>
2019-06-13 08:23:55 +0530

    commit1

---
+++ spring-cloud-config/src/main/java/com/in28minutes/microservices/springcloudconfig/SpringCloudConfigApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.microservices.springcloudconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigApplication.class, args);
+	}
+
+}
